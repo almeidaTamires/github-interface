6f7316d4d94990f1ccd8ab52c9d8db337f6ab99f
Tamires <tamires_knowles@hotmail.com>
2022-06-10 17:38:14 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Layout>
+      <div>
+        <img source="https://avatars.githubusercontent.com/u/99164770?v=4" alt="picture of user"/>
+        <h1>Tamires Almeida</h1>
+        <h3>UserName: </h3>
+        <span>almeidaTamires</span>
+        <div>
+          <div>
+            <h4>Follower</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>5</span>
+          </div>
+        </div>
+      </div>
+
+      <div>Repositories</div>
+      <div>Starreds</div>
+    </Layout>
+    
+    </main>
   );
 }
 
